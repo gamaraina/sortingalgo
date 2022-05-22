6ea9feb4b8329a8b7ac0702ec5e1801f3bbc63dd
gamaraina <57658819+gamaraina@users.noreply.github.com>
2022-05-22 21:34:03 +0530

    added procfile

---
+++ App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import Sortingv from './SortingV/Sortingv';
+import './App.css';
+import { Footer } from './Footer';
+
+
+function App() {
+  return (
+    <>
+
+      <div className="App beautify">
+        <div class="beauty">
+          <h1>SORTING ALGO</h1>
+          <h2></h2>
+
+          <Sortingv></Sortingv>
+          <Footer />
+        </div>
+
+      </div>
+
+    </>
+
+  );
+}
+
+export default App;
